77f6b728c7c70f6d1695001a65aec06a69bab34e
Parshuram Kumar <yadavparshuram991@gmail.com>
2025-07-19 03:42:03 +0530

    changed post form ui

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -4,10 +4,10 @@ import PostForm from "../components/post-form/postForm";
 
 function AddPost() {
   return (
-    <section className="min-h-[80vh] flex items-center justify-center bg-gradient-to-b from-gray-50 via-white to-gray-100 px-4">
+    <section className="min-h-[80vh] flex items-center justify-center  px-4">
       <Container>
-        <div className="max-w-2xl w-full bg-white rounded-2xl shadow-md p-6 sm:p-10 border border-gray-200">
-          <h1 className="text-2xl sm:text-3xl font-semibold text-gray-800 mb-6 text-center">
+        <div className=" w-full  rounded-2xl  p-6 sm:p-10 ">
+          <h1 className="text-3xl sm:text-3xl font-semibold text-gray-800 mb-6 text-center">
             Create a New Post ✍️
           </h1>
           <PostForm />
